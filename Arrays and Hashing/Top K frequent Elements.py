f17c4d712fa886c99a469938ea63cb7aa73418dd
glauuucoma <kulebadavid@gmail.com>
2024-02-23 23:36:46 -0500

    Fix previous solution

--- Arrays and Hashing/Top K frequent Elements.py
+++ Arrays and Hashing/Top K frequent Elements.py
@@ -1,18 +1,15 @@
 def topKFrequent(nums, k):
-# Create a dictionary that counts occurence of numbers in array
-# Create an array that will keep k most frequent elements
-# Each time a dictionary value is assigned check occurence and compare
-    occur = {}
-    frequent = []
+    count = {}
+    freq = [[] for i in range(nums) + 1]
 
-    for num in nums:
-        occur[num] = occur.get(num, 0) + 1  # Register occurence 
-        if len(frequent) < k and num not in frequent: # If frequent doesn't have enough numbers add numbers to it
-            frequent.append(num) # Add number to it
-        elif len(frequent) == k: # Else if length == k
-            for i in frequent: # For each frequent element
-                if occur[i] < occur[num]: # If occurences of array element is less than current element
-                    frequent.pop(i) # Remove less element from array
-                    frequent.append(num) # Add mode occured element
-            
-    return frequent
+    for n in nums:
+        count[n] = 1 + count.get(n,0)
+    for n,c in count.items(): # Key value pair
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
