06d15599a166cb7ec1567a106b3db17c48698142
glauuucoma <kulebadavid@gmail.com>
2024-02-25 22:17:56 -0500

    solve best time to buy stock problem

---
+++ Sliding Window/Best time to buy or sell stock.py
@@ -0,0 +1,13 @@
+def maxProfit(prices):
+    left, right = 0, 1
+    max_profit = 0
+
+    while right < len(prices):
+        current_profit = prices[right] - prices[left]
+        if prices[left] < prices[right]:
+            max_profit = max(current_profit, max_profit)
+        else:
+            left = right 
+        right += 1
+
+    return max_profit
