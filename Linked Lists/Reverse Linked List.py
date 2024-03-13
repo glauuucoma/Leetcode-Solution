2670bfd694fd26f3e4d3bbb5de62918f31e6ca8a
glauuucoma <kulebadavid@gmail.com>
2024-03-12 23:23:32 -0400

    classify solutions

--- Linked Lists/Reverse Linked List.py
+++ Linked Lists/Reverse Linked List.py
@@ -1,17 +1,10 @@
 def reverseList(self, head):
-    prev, curr = None, head
-
-    while curr:
-        nxt = curr.next # Temprorarily variable
-        curr.next = prev
-        prev = curr
-        curr= nxt
-    return prev
-
-# Break down
-
-# Initialize previous and current Node
-# Previous is None and the curr is head
-# While node exists:
-# Next node is curr.next property
-# 
+        # Use two pointers: prev-curr
+        # T O(n), Space - O(1)
+    prev, curr = None, head # Initialize pointers
+    while curr: # Check if node exists
+        nxt = curr.next # Temp. var to have next value
+        curr.next = prev # Setting pointer of curr value to previous
+        prev = curr # Move prev pointer to curr
+        curr = nxt # Move curr pointer to next
+    return prev # Return the whole restructured node
